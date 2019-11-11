e44e3b684e3ac2928dd99f4193d8f0c011a667c6
Anton Melnikov <hello@antonmelnikov.com>
2019-11-11 17:27:00 -0600

    Restore comment

--- mockfirestore/__init__.py
+++ mockfirestore/__init__.py
@@ -1,3 +1,7 @@
+# by analogy with
+# https://github.com/mongomock/mongomock/blob/develop/mongomock/__init__.py
+# try to import gcloud exceptions
+# and if gcloud is not installed, define our own
 try:
     from google.cloud.exceptions import ClientError, Conflict, AlreadyExists
 except ImportError:
