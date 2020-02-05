4fd4d1461486e6e8d78252deaf9e4e2650bfe0f5
Billcountry <me@billcountry.tech>
2020-02-05 09:29:47 +0300

    Array contains queries implementation

--- mockfirestore/query.py
+++ mockfirestore/query.py
@@ -126,3 +126,7 @@ class Query:
             return lambda x, y: x >= y
         elif op == 'in':
             return lambda x, y: x in y
+        elif op == 'array_contains':
+            return lambda x, y: y in x
+        elif op == 'array_contains_any':
+            return lambda x, y: any([val in y for val in x])
