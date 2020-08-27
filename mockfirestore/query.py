b1ffcf41e8b46a0610cc8b3eb4cdaf5e182aaf74
kromash <k.romaszko@sotrender.com>
2020-08-27 16:30:57 +0200

    Fix _apply_cursor for first element

--- mockfirestore/query.py
+++ mockfirestore/query.py
@@ -103,7 +103,7 @@ class Query:
                     index = idx
                 else:
                     index = None
-            if index:
+            if index is not None:
                 if before and start:
                     return islice(docs, index, None, None)
                 elif not before and start:
