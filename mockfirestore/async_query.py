a29ed6a0420d1c3e68a859fe72b11b2fbf713a45
Ben van der Harg <ben.vanderharg@incentro.com>
2022-06-02 15:02:55 +0200

    Replace async list comprehension with existing consume_async_iterable

--- mockfirestore/async_query.py
+++ mockfirestore/async_query.py
@@ -1,11 +1,11 @@
-from typing import List, AsyncGenerator
+from typing import List, AsyncGenerator, AsyncIterator
 from mockfirestore.document import DocumentSnapshot
 from mockfirestore.query import Query
 from mockfirestore._helpers import consume_async_iterable
 
 
 class AsyncQuery(Query):
-    async def stream(self, transaction=None) -> AsyncGenerator[DocumentSnapshot]:
+    async def stream(self, transaction=None) -> AsyncIterator[DocumentSnapshot]:
         doc_snapshots = await consume_async_iterable(self.parent.stream())
         doc_snapshots = super()._process_field_filters(doc_snapshots)
         doc_snapshots = super()._process_pagination(doc_snapshots)
@@ -13,4 +13,4 @@ class AsyncQuery(Query):
             yield doc_snapshot
 
     async def get(self, transaction=None) -> List[DocumentSnapshot]:
-        return [result async for result in self.stream()]
+        return await consume_async_iterable(self.stream())
