48966588cbb7c4769d836805f3ae51cef27f7d01
Andrei Gherasim <andrei@gherasim.xyz>
2023-07-10 21:43:00 +0200

    Further fixes for missing features: transactions in DocumentReference; path property in DocumentReference

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mock-firestore",
-    version="0.11.0",
+    version="0.11.3",
     author="Matt Dowds",
     description="In-memory implementation of Google Cloud Firestore for use in tests",
     long_description=long_description,
